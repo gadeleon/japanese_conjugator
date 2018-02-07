a16d62c3c7e65b7c2a6faff2132299806449c0aa
Gabriel Deleon <notprivategabe@gmail.com>
2018-02-07 00:27:54 -0500

    first part of passive question

--- questions.py
+++ questions.py
@@ -4,3 +4,13 @@ import random
 
 import conjugations
 import vocab.japanese_verbs as verbs
+
+def passive_question():
+	styles = {
+		'godan':conjugations.godan_passive,
+		'ichidan':conjugations.ichidan_pot_pas,
+		'irregular':conjugations.irregular_passive
+	}
+	verbstage = random.choice(styles.keys())
+	verb = random.choice(verbs.verb1[verbstage].keys())
+	return verb
