d4b736a1e0218bd52aedaadd55b9a9d7d1480459
Gabriel Deleon <gad265@Gabriels-MBP.fios-router.home>
2018-02-06 23:54:27 -0500

    added verbs

--- vocab/japanese_verbs.py
+++ vocab/japanese_verbs.py
@@ -1,3 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
 '''
-Just a hash full of verbs. Was thinking of pickling but this would be easier.
+Just a hash of verbs. Was thinking of pickling but this would be easier.
 '''
+
+
+verb1 = {
+    'godan': {
+        '置く':'to put, lay, place',
+        '気が付く' : 'to notice',
+        '蹴る' : 'to kick',
+        '刺す' : 'to bite',
+        '触る': 'to touch',
+        '捕まる': 'to be arrested; caught',
+        '包む':'to wrap',
+        '殴る':'to punch',
+        '盗む' : 'to steal',
+        '貼る' : 'to post',
+        '踏む' : 'to step on',
+        '振る': '1) to shake, 2) to turn down',
+        '文句を言う': 'to complain',
+        '焼く': 'to bake, burn',
+        '遣る': '1) to do, 2) to give to someone of lower status (pets, plants, kids, etc), and way more... it\'s *that* verb ',
+        'ふる': 'to turn down',
+        'やる': 'to give to someone of lower status'
+        },
+    'ichidan': {
+        'いじめる': 'to bully',
+        'きがえる': 'to change clothes',
+        '貯める':'to save money',
+        '続ける': 'to continue',
+        '褒める': 'to praise',
+        '間違える': 'to make a mistake',
+        '見つける': 'to find',
+        },
+    'irregular': {
+        '馬鹿にする': 'to make a fool of, to insult',
+        'びっくりする': 'to be surprised',
+        '昼寝をする': 'to take a nap',
+        '連絡する': 'to contact' 
+        }
+
+
+
+
+}
