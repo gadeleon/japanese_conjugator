299d2a4b42884cf7fc56b9e2d5d55c11758b13a1
Gabriel Deleon <notprivategabe@gmail.com>
2018-05-14 07:35:01 -0400

    started work on godan verbs

--- words.py
+++ words.py
@@ -11,6 +11,22 @@ class Word(object):
     def __str__(self):
         return self.word
 
-    def __repr__(self):
+    # def __repr__(self):
+    #     return self.word
+
+
+class GodanVerb(Word):
+    def __init__(self, word):
+        super(GodanVerb, self).__init__(word)
+        self.stem = self._get_stem(word)
+
+
+    def __str__(self):
         return self.word
 
+    def _get_stem(self, word):
+        syl = word.decode('utf-8')[-1]
+        base = u'{}'.format(word.decode('utf-8')[:-1])
+        if syl == u'つ':
+            stem = u'{}ち'.format(base)
+        return stem
