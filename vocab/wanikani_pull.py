6e3732b00d01065d815a39ce9636d2296bcc5408
Gabriel Deleon <notprivategabe@gmail.com>
2018-10-30 20:01:01 +0900

    added a get_word function to deprecate  get_noun

--- vocab/wanikani_pull.py
+++ vocab/wanikani_pull.py
@@ -32,5 +32,23 @@ def _grab_noun():
         #pprint(word)
     return word['data']['slug']
 
+def _grab_word(partofspeech):
+    '''
+    Randomly picks a word from wanikani's extensive vocab, If partofspeech matches (ie verb, noun, etc),
+    then it returns
+    :param partofspeech: ichidan_vorb, na_adjective, i_adjective, etc.
+    :return:
+    '''
+    word = random.choice(src['data'])
+    pprint(word)
+    while partofspeech not in word['data']['parts_of_speech']:
+        word = random.choice(src['data'])
+        pprint(word)
+    return word['data']['slug']
+
 no = _grab_noun()
 print(no)
+v = _grab_word('i_adjective')
+print(v)
+n = _grab_word('noun')
+print(n)
