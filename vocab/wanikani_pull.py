0d1a737beeb0be507707d680380419f9cc2a31ab
Gabriel Deleon <notprivategabe@gmail.com>
2018-10-30 19:55:47 +0900

    added noun extraction

--- vocab/wanikani_pull.py
+++ vocab/wanikani_pull.py
@@ -4,9 +4,33 @@ Grab a word from wanikani cache
 
 import json
 import pickle
+import random
 
 cache = pickle.load(open('wanikanicache.pkl', 'rb'))
 
 data = cache.text
 
-print(data)
+src = json.loads(data)
+
+from pprint import pprint
+
+#pprint(src)
+
+# Desired Kanji is in data[N][data['slug']
+# Adj/Verb/Noun in data[N][data['parts_of_speech']
+
+
+def _grab_noun():
+    '''
+    Randomly picks a noun from wanikani's vocab
+    :return: noun
+    '''
+    word = random.choice(src['data'])
+    #pprint(word)
+    while 'noun' not in word['data']['parts_of_speech']:
+        word = random.choice(src['data'])
+        #pprint(word)
+    return word['data']['slug']
+
+no = _grab_noun()
+print(no)
