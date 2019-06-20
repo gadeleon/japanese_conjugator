bc64fe473c8a18b283e773d66d33dcf544feab52
Gabriel Deleon <notprivategabe@gmail.com>
2019-06-20 22:48:37 +0900

    first function and init

--- grammar/n4grammar.py
+++ grammar/n4grammar.py
@@ -2,3 +2,11 @@
 N4 level grammar constructions
 '''
 
+def positive_comparison_extent(strong_noun, weak_noun, comparison):
+	'''
+	Produces the grammar where you compare two nouns. 
+	Effictively stroung_noun is more comparison than weak_noun
+	This is NOT an expression of preference.
+	Generally the strong noun is the topic of the sentence.
+	'''
+	return '{}は{}{}'.format(strong_noun, weak_noun, comparison)
